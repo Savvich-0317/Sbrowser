e89c6ffeb4e304b05803402bee4e3a66ab9441ea
Savvich <savvaglinskij74@gmail.com>
2025-05-28 23:13:35 +0300

    minor

--- Sbrowser/Form1.Designer.cs
+++ Sbrowser/Form1.Designer.cs
@@ -28,28 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1.SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
-            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabPage1
+            // textBox1
             // 
-            this.tabPage1.Controls.Add(this.textBox1);
-            this.tabPage1.Controls.Add(this.button1);
-            this.tabPage1.Controls.Add(this.webView21);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(987, 520);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(924, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(932, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 20);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // webView21
             // 
@@ -59,68 +63,33 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.webView21.CreationProperties = null;
             this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.webView21.Location = new System.Drawing.Point(3, 32);
+            this.webView21.Location = new System.Drawing.Point(2, 28);
             this.webView21.Name = "webView21";
-            this.webView21.Size = new System.Drawing.Size(981, 485);
-            this.webView21.TabIndex = 0;
+            this.webView21.Size = new System.Drawing.Size(991, 533);
+            this.webView21.TabIndex = 3;
             this.webView21.ZoomFactor = 1D;
-            this.webView21.Click += new System.EventHandler(this.webView21_Click);
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(933, 6);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(51, 20);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Go!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(3, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(924, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(995, 546);
-            this.tabControl1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(995, 546);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(995, 563);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webView21);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
-            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
-        private System.Windows.Forms.TabControl tabControl1;
     }
 }
 
--- Sbrowser/Form1.cs
+++ Sbrowser/Form1.cs
@@ -8,26 +8,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace Sbrowser
 {
+
+    
+    
+
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-            tabPage1.Name = "Page1";
+            Timer timer;
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += UpdateAdress;
+            timer.Start();
+
         }
 
-        private void webView21_Click(object sender, EventArgs e)
+        private void UpdateAdress(object sender, EventArgs e)
         {
+            textBox1.Text = Convert.ToString(webView21.Source);
         }
 
-        
 
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
-        {
-            
-        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -35,9 +44,28 @@ namespace Sbrowser
             {
                 webView21.Source = new Uri(textBox1.Text);
             }
-            catch { 
-                textBox1.Text = "Error";
+            catch {
+                if (textBox1.Text.Contains("https://"))
+                {
+                    textBox1.Text = "Adress not responding, or not valid";
+                }
+                else
+                {
+                    try
+                    {
+                        webView21.Source = new Uri("https://"+textBox1.Text);
+                        textBox1.Text = "https://" + textBox1.Text;
+                    }
+                    catch {
+
+                        textBox1.Text = "Adress not responding, or not valid";
+
+                    }
+                }
+               
             }
         }
+
+        
     }
 }
