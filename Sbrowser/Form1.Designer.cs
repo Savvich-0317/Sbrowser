c938ddaa3fd55ea5afb3bc24da8c30196af4084d
Savvich <savvaglinskij74@gmail.com>
2025-05-29 15:01:12 +0300

    minor loading sign

--- Sbrowser/Form1.Designer.cs
+++ Sbrowser/Form1.Designer.cs
@@ -28,10 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -41,15 +44,16 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Location = new System.Drawing.Point(2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(924, 20);
+            this.textBox1.Size = new System.Drawing.Size(836, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(932, 2);
+            this.button1.Location = new System.Drawing.Point(862, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(61, 20);
+            this.button1.Size = new System.Drawing.Size(68, 20);
             this.button1.TabIndex = 5;
             this.button1.Text = "Go!";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,23 +69,39 @@
             this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
             this.webView21.Location = new System.Drawing.Point(2, 28);
             this.webView21.Name = "webView21";
-            this.webView21.Size = new System.Drawing.Size(991, 533);
+            this.webView21.Size = new System.Drawing.Size(928, 418);
             this.webView21.Source = new System.Uri("https://google.com", System.UriKind.Absolute);
             this.webView21.TabIndex = 3;
             this.webView21.ZoomFactor = 1D;
+            this.webView21.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webView21_NavigationCompleted);
             this.webView21.SourceChanged += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2SourceChangedEventArgs>(this.webView21_SourceChanged);
+            this.webView21.ContentLoading += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2ContentLoadingEventArgs>(this.webView21_ContentLoading);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(838, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(995, 563);
+            this.ClientSize = new System.Drawing.Size(932, 448);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.webView21);
             this.Name = "Form1";
             this.Text = "SBrowser";
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,6 +112,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Sbrowser/Form1.cs
+++ Sbrowser/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,6 @@ using System.Windows.Forms;
 
 namespace Sbrowser
 {
-
-    
     
 
 
@@ -29,11 +28,22 @@ namespace Sbrowser
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            
+            SendAdress();
+        }
+
+        private void SendAdress()
+        {
+            
+
             try
             {
+                
                 webView21.Source = new Uri(textBox1.Text);
             }
-            catch {
+            catch
+            {
                 if (textBox1.Text.Contains("https://"))
                 {
                     textBox1.Text = "Adress not responding, or not valid";
@@ -42,25 +52,48 @@ namespace Sbrowser
                 {
                     try
                     {
-                        webView21.Source = new Uri("https://"+textBox1.Text);
+                        
+                        webView21.Source = new Uri("https://" + textBox1.Text);
                         textBox1.Text = "https://" + textBox1.Text;
                     }
-                    catch {
+                    catch
+                    {
 
                         textBox1.Text = "Adress not responding, or not valid";
 
                     }
                 }
-               
+
             }
         }
 
         private void webView21_SourceChanged(object sender, Microsoft.Web.WebView2.Core.CoreWebView2SourceChangedEventArgs e)
         {
+            
+
             if (textBox1.Text != webView21.Source.ToString())
             {
+                
                 textBox1.Text = webView21.Source.ToString();
             }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) {
+
+                SendAdress();
+            }
+        }
+
+        private void webView21_ContentLoading(object sender, Microsoft.Web.WebView2.Core.CoreWebView2ContentLoadingEventArgs e)
+        {
+            pictureBox1.Visible = true;
+        }
+
+        private void webView21_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            pictureBox1.Visible = false;
         }
     }
 }
